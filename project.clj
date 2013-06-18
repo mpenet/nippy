27000f2029adeff1114ab0f28e5527a92410bbe0
Max Penet <m@qbits.cc>
2013-06-18 21:40:53 +0200

    clojars

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.taoensso/nippy "2.0.0-beta1"
+(defproject cc.qbits/nippy "2.0.0-beta1"
   :description "Clojure serialization library"
   :url "https://github.com/ptaoussanis/nippy"
   :license {:name "Eclipse Public License"
